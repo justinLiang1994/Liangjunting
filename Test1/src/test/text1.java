b9c4ab07703fea90581d91bad252fd159852e6b4
Administrator <Administrator@tarena>
2020-01-03 11:07:18 +0800

    122

--- Test1/src/test/text1.java
+++ Test1/src/test/text1.java
@@ -3,6 +3,7 @@ package test;
 public class text1 {
 				public static void main(String[] args) {
 					String s ="abc";
+					System.out.println(s);
 					System.out.println("4564");
 					System.out.println("fdafad");
 					System.out.println("fdsafa");
