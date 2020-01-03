559d53e3e361b4d9c51c27e88dad976731a71cbe
Administrator <Administrator@tarena>
2020-01-03 11:01:51 +0800

    ds

--- Test1/src/test/text1.java
+++ Test1/src/test/text1.java
@@ -2,7 +2,10 @@ package test;
 
 public class text1 {
 				public static void main(String[] args) {
+					String s ="abc";
 					System.out.println("4564");
 					System.out.println("fdafad");
+					System.out.println("fdsafa");
 				}
+				
 }
